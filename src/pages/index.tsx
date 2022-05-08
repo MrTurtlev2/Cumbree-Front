a59a25e62d8e73efc70043f64e8fb7e9fdd4fa2c
Marek <wojnarmarek0@gmail.com>
2022-05-08 16:13:49 +0200

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import Layout from '../components/common/Layout';
+import { getRecipesAsync, selectRecipes } from '../features/recipesSlice';
 
 const IndexPage = () => {
+
+	const recipesArray = useSelector(selectRecipes);
+	const dispatch = useDispatch();
+
+	useEffect(()=> {
+		dispatch(getRecipesAsync());
+	}, []);
 	
 	return (
 		<Layout>
 			<PageWrapper>
 				to jest index 
+				{recipesArray.map((item)=> <span key={item.id}>{item.title}</span>)}
 			</PageWrapper>			
 		</Layout>
 	);
